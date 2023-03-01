594833a3982c5d950519d59233f9f50cf08fd6f4
PhilippePH <55930314+PhilippePH@users.noreply.github.com>
2023-03-01 20:55:12 +0000

    oops

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -125,9 +125,9 @@ class Regressor():
         x = pd.concat([x, binarised_ocean_proximity], axis=1) # adding the 5 dummy columns
 
         # REMOVING NA VALUES FROM X
-        print("BEFORE FILL NA:", np.sum(np.isnan(X)))
+        print("BEFORE FILL NA:", np.sum(np.isnan(x)))
         x = x.fillna(0)
-        print("AFTER FILL NA:", np.sum(np.isnan(X)))
+        print("AFTER FILL NA:", np.sum(np.isnan(x)))
 
         # IF TESTING, USE STORED PREPROCESSED ATTRIBUTES FOR X
         if not training:
